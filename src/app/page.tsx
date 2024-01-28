f6e78649d4d4f2c1a91c5b15d9c216528bccc9bb
elinlqv <elinlqv@gmail.com>
2024-01-28 20:37:44 +0100

    make comparison graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,197 @@
-import Image from "next/image";
+//import LineChart from './linechart';
 
-export default function Home() {
+"use client";
+import "./globals.css";
+const categories = [
+  "viz",
+  "stats",
+  "math",
+  "art",
+  "ui",
+  "code",
+  "graph",
+  "hci",
+  "eval",
+  "comm",
+  "collab",
+  "GIT",
+];
+
+import React, { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  Point,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import * as d3 from "d3";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+let hovering = false,
+  tooltips = ["such tooltip", "blah blah"];
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+      onHover: function (event, legendItem) {
+        if (hovering) {
+          return;
+        }
+        hovering = true;
+        let tooltip = document.getElementById("tooltip");
+        tooltip.innerHTML = tooltips[legendItem.datasetIndex];
+        tooltip.style.left = event.x + "px";
+        tooltip.style.top = event.y + "px";
+      },
+      onLeave: function () {
+        let tooltip = document.getElementById("tooltip");
+        hovering = false;
+        tooltip.innerHTML = "";
+      },
+    },
+  },
+};
+
+export default function App() {
+  const [allData, setAllData] = useState<Record<string, any>[]>();
+  const [chartData, setChartData] =
+    useState<ChartData<"line", (number | Point | null)[], unknown>>();
+  const [datasets, setDatasets] = useState<any[]>();
+
+  useEffect(() => {
+    async function getData() {
+      const all_data_rows = await d3.csv("/input.csv");
+      setAllData(all_data_rows);
+
+      const datasets = all_data_rows.map((rowObject) => ({
+        label: rowObject["ALIAS"],
+        data: Object.keys(rowObject)
+          .filter((key) => categories.includes(key))
+          .map((key) => rowObject[key]), // skapar en lista med nyckelpar
+        borderColor: "rgb(255, 99, 255)",
+        // options:{
+        //   legend:{onHover((rowObject)=> rowObject[]) }
+        // };
+        // backgroundColor: "rgba(255, 99, 132, 0.5)",
+      }));
+
+      setDatasets(datasets);
+      setChartData({
+        labels: categories,
+        datasets: [],
+      });
+    }
+    getData();
+  }, []);
+
+  function calculateAndSetChartData(yourRowIndex: number) {
+    // TODO, calculate youself and three other datasets
+
+    const numeric_dataset = allData
+      ?.filter((_, index) => index != yourRowIndex)
+      .map((rowObject) =>
+        Object.keys(rowObject)
+          .filter((key) => categories.includes(key))
+          .map((key) => Number(rowObject[key]))
+      );
+    //todo filter yourself
+    console.log(numeric_dataset);
+
+    //gör om "migsjälv till samma format som numeric dataset
+    const me_numeric = Object.keys(allData[yourRowIndex])
+      .filter((key) => categories.includes(key))
+      .map((key) => Number(allData[yourRowIndex][key]));
+
+    console.log(me_numeric);
+    //för varje person i numeric dataset
+    //räkna ut differensen mellan värde och värdet jag själv har på samma plats.
+    //addera differenserna
+    //spara differensen med personens index i en array
+    const index_and_difference = [];
+    for (let i = 0; i < numeric_dataset.length; i++) {
+      let accumulated_difference = 0;
+
+      for (let j = 0; j < me_numeric.length; j++) {
+        accumulated_difference += Math.min(
+          me_numeric[j] - numeric_dataset[i][j],
+          0
+        );
+      }
+      index_and_difference.push({
+        index: i,
+        accumulated_difference: accumulated_difference,
+      });
+    }
+
+    //sortera arrayen: högst differens först
+    console.log(index_and_difference);
+    index_and_difference.sort(
+      (a, b) => a.accumulated_difference - b.accumulated_difference
+    );
+    console.log(index_and_difference);
+
+    //plocka ut indesen av de tre första i den sorterade arrayen
+
+    //rendera deras linjer.
+    const datasetsWithoutMe = datasets.filter(
+      (_, index) => index != yourRowIndex
+    );
+    const otherDataset = {
+      ...datasetsWithoutMe[index_and_difference[0].index],
+      borderColor: "rgb(255, 0, 0)",
+    };
+    const otherDataset2 = {
+      ...datasetsWithoutMe[index_and_difference[1].index],
+      borderColor: "rgb(0, 255, 0)",
+    };
+    const otherDataset3 = {
+      ...datasetsWithoutMe[index_and_difference[2].index],
+      borderColor: "rgb(0, 0, 255)",
+    };
+    setChartData((current) => ({
+      ...current,
+      datasets: [
+        datasets[yourRowIndex],
+        otherDataset,
+        otherDataset2,
+        otherDataset3,
+      ],
+    }));
+  }
+
+  if (!chartData || !allData) return "loading...";
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="maindiv">
+      <h1>Let's find you some teammates!</h1>
+      <h3> 1. Select your Alias:</h3>
+      <select
+        onChange={(e) => calculateAndSetChartData(e.target.value as number)}
+      >
+        {allData.map((row, i) => (
+          <option value={i}>{row["ALIAS"]}</option>
+        ))}
+      </select>
+      <h3> 2. These three complement your skills the best:</h3>
+      <Line options={options} data={chartData} />
+      <div id="tooltip" className="tooltip"></div>
+    </div>
   );
 }
